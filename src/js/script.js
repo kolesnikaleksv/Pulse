15dddcb5808df38cc02c7003cf48109021d53070
kolesnikaleks <kolesnikaleksv@gmail.com>
2020-05-22 09:55:05 +0300

    page up

--- src/js/script.js
+++ src/js/script.js
@@ -117,6 +117,21 @@ $(document).ready(function(){
         });
         return false;
       });
+
+      // Smooth scroll and pageup
+
+      $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+          $('.page-up').fadeIn();
+        } else {
+          $('.page-up').fadeOut();
+        }
+      });
+      $("a[href^='#up']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+      });
   });
 
 const slider = tns({
